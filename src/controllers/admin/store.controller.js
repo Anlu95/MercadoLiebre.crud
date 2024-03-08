31bf402d263f06ed9c805b827c594d0896fc8a19
AntonelaDuckwen <antoduckwen@gmail.com>
2024-03-08 18:48:39 -0300

    subo los cambios

---
+++ src/controllers/admin/store.controller.js
@@ -0,0 +1,20 @@
+const { loadData } = require("../../data")
+
+module.exports = (req,res) => {
+    const {name, price, discount, description,category } = req.body;
+    const products = loadData()
+    const newID = products[products.length - 1 ].id + 1
+    const newproduct = {
+        id: newID,
+        name:name.trim(), 
+        price: +price, 
+        discount: +discount, 
+        description:description.trim(),
+        category:category?.trim(),
+        image:"default-image.png"
+    };
+
+    products.push(newProduct)
+    saveData(products)
+    res.redirect(`/productos/detalle/${newID}`);
+};
